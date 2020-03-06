b666fa5c2b5b887721fc6a8d6cd2432ad0b88d21
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-03-06 18:38:21 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if fizzbuzz(int) % 3
+  if int % 3
     puts "Fizz"
-  elsif fizzbuzz(int) % 5
+  elsif int % 5
     puts "Buzz"
-  elsif fizzbuzz(int) % 3 && fizzbuzz(int) % 5
+  elsif int % 3 && int % 5
     puts "FizzBuzz"
   else
     puts "nil"
