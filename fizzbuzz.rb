8773a15ebc2f699433480bd47e8f05e0ad794a83
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-03-06 18:36:35 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,9 +5,9 @@ fizz_int = fizzbuzz(int)
 def fizzbuzz(int)
   if int % 3
     puts "Fizz"
-  elsif int % 5 == 0
+  elsif int % 5
     puts "Buzz"
-  elsif int % 3 == 0 && int % 5 == 0
+  elsif int % 3 && int % 5
     puts "FizzBuzz"
   else
     puts "nil"
