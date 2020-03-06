5656bd66666822a193cc43857cabbd7cfccd1329
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-03-06 18:34:22 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 3
+  if fizz_3 % 3
     puts "Fizz"
   elsif int % 5 ==0
     puts "Buzz"
